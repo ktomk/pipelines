a398f09eb3dda6a040258137c4339dd7c9396172
Tom Klingenberg <tklingenberg@lastflood.net>
2019-12-19 12:11:59 +0100

    add busybox image for bitbucket pipelines
    while testing pipelines on bitbucket with the official ducker hub busybox
    image, the atlassian bitbucket cloud pipeline plugin ran into an error
    while setting up the build [REPORT]:
    
        /bin/sh: /usr/bin/mkfifo: not found
    
    to work around this on atlassian bitbucket a busybox image that is just
    the busybox image only modified w/ a symbolic link to prevent the error
    has been created and uploaded.
    
    in this change set is the script to build and push the image as well making
    use of it in the projects' pipelines.
    
    this should make pass the build again on atlassian bitbucket cloud.
    
    it might be that w/ future changes on atlassian bitbucket cloud the
    original image is supported (again).
    
    REPORT: https://community.atlassian.com/t5/Bitbucket-questions/Pipeline-Build-Setup-bin-sh-usr-bin-mkfifo-not-found/qaq-p/1253290

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to Pipelines will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [unreleased]
+### Fixed
+- Busybox on Atlassian Bitbucket Cloud
+
 ## [0.0.23] - 2019-12-17
 ### Changed
 - Improve `--help` display
---
+++ lib/containers/busybox.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# this file is part of pipelines
+#
+# pipelines:busybox docker image
+#
+# usage: ./busybox.sh [<docker-cmd>]
+#
+#   <docker-cmd>    "docker" by default
+#
+# requirements: docker (or equivalent)
+#
+# rationale:
+#
+#   bitbucket pipelines runs the build container creating a named pipe and
+# open it for reading exiting non-zero in case of error w/ this command:
+#
+#     /bin/sh -c exit $( \
+#       ( /usr/bin/mkfifo /opt/atlassian/pipelines/agent/tmp/build_result \
+#         && /bin/cat /opt/atlassian/pipelines/agent/tmp/build_result) \
+#       || /bin/echo 1)
+#
+#   (formated for readability)
+#
+#   as the official docker hub image of busybox does not have the mkfifo
+# utility at /usr/bin/mkfifo, the build setup errors and the pipeline step/s
+# are never run:
+#
+#     /bin/sh: /usr/bin/mkfifo: not found
+#
+#   therefore to be able to use a close to vanilla busybox container on
+# bitbucket pipelines at the time of writing the image needs to have the mkfifo
+# utility linked at /usr/bin/mkfifo - it is at /bin/mkfifo
+#
+#   the reason why bitbucket pipelines uses /usr/bin here could not be
+# clarified at time of writing, it uses normally the utilities from /bin
+# like /bin/sh, /bin/cat, ... .
+#
+# <https://community.atlassian.com/t5/Bitbucket-questions/Pipeline-Build-Setup-bin-sh-usr-bin-mkfifo-not-found/qaq-p/1253290>
+#
+set -euo pipefail
+IFS=$' \n\t'
+
+docker_cmd="${1-docker}"
+from="docker.io/busybox"
+tag="docker.io/ktomk/pipelines:busybox"
+
+echo "build '${tag}' from '${from}' with ${docker_cmd}..."
+
+<<'DOCKERFILE' "${docker_cmd}" build --build-arg FROM="${from}" -t "${tag}" - | sed -e 's/^/  build: /'
+ARG FROM
+FROM $FROM
+RUN set -eux; \
+  : link /usr/bin/mkfifo for atlassian bitbucket cloud pipeline plugin; \
+    mkdir -p /usr/bin; \
+    ln -s /bin/busybox /usr/bin/mkfifo;
+DOCKERFILE
+
+echo "push '${tag}' ..."
+
+"${docker_cmd}" push "${tag}" | sed -e 's/^/  push.: /'
+
+echo "done."
