23ba205ffaea8877c3c79bef9e4e2967a5cf4dcc
Tom Klingenberg <tklingenberg@lastflood.net>
2019-12-24 14:15:32 +0100

    add test
    keep and reuse container.

--- tests/shell/test-polish.sh
+++ tests/shell/test-polish.sh
@@ -9,13 +9,14 @@ IFS=$'\n\t'
 #
 #               TEST PLAN
 #
+# [ 3] keep and reuse container
 # [ 2] run w/ script generation and verbose output (w/ services: - docker)
 # [ 1] run w/ script generation and verbose output
 #
 
 case ${1-0} in
   0 ) echo "# 0: ${0} run"
-      run_test "${0}" 2 1
+      run_test "${0}" 3 2 1
       exit
       ;;
   1 ) echo "# 1: run w/ script generation and verbose output"
@@ -26,6 +27,16 @@ case ${1-0} in
   2 ) echo "# 2: run w/ script generation and verbose output (w/ services: - docker)"
       # shows dry-run issue w/ docker client install:
       ../../bin/pipelines -v --dry-run --pipeline custom/unit-tests
+      exit
+      ;;
+  3 ) echo "# 3: keep and reuse container"
+      ../../bin/pipelines --docker-zap >/dev/null
+      ../../bin/pipelines --keep | grep 'keeping container id'
+      docker ps | grep 'pipelines-1.pipeline-features-and-introspection.default.pipelines'
+      ../../bin/pipelines --keep | grep 'keeping container id'
+      docker ps | grep 'pipelines-1.pipeline-features-and-introspection.default.pipelines'
+      ../../bin/pipelines | tail -1 | grep -v 'keeping container id'
+      docker ps | grep -v 'pipelines-1.pipeline-features-and-introspection.default.pipelines'
       exit
       ;;
   * ) >&2 echo "unknown step ${1}"
