74428b96ed00cbdee317a62d64bc60ec058d6a64
Tom Klingenberg <tklingenberg@lastflood.net>
2021-11-07 14:09:53 +0100

    development xdebug loader xdebug3 support
    improvement of 350b7be with xdebug3 step-debugging parameters.
    
    ref: 350b7be5316bec0e41987d358af4e0023cccb385

--- bin/dev/xdebug.php
+++ bin/dev/xdebug.php
@@ -3,7 +3,7 @@
 /*
  * pipelines - run bitbucket pipelines wherever they dock
  *
- * Copyright 2020 Tom Klingenberg <ktomk@github.com>
+ * Copyright 2020, 2021 Tom Klingenberg <ktomk@github.com>
  *
  * Licensed under GNU Affero General Public License v3.0 or later
  *
@@ -14,23 +14,37 @@
  */
 
 if ($result = array_keys($argv, '--xdebug', true)) {
-    $util = $argv[0];
-    $name = basename($util);
+    $utilPathname = $argv[0];
+    $name = basename($utilPathname);
+    $serverName = "$name-cli";
     unset($argv[0], $argv[$result[0]]);
     $cmd = Ktomk\Pipelines\Lib::cmd(Ktomk\Pipelines\Lib::phpBinary(), array(
         extension_loaded('xdebug') ? null : '-dzend_extension=xdebug.so',
-        '-dxdebug.remote_enable=1', '-dxdebug.remote_mode=req', '-dxdebug.remote_port=9000',
-        '-dxdebug.remote_host=127.0.0.1', '-dxdebug.remote_connect_back=0',
-        '-f', $util, '--',
+        /* xdebug 2 */
+        (false) ? array(
+            '-dxdebug.remote_enable=1',
+            '-dxdebug.remote_mode=req',
+            '-dxdebug.remote_host=127.0.0.1',
+            '-dxdebug.remote_port=9000',
+            '-dxdebug.remote_connect_back=0',
+        /* xdebug 3 */
+        ) : array(
+            '-dxdebug.mode=debug',
+            '-dxdebug.start_with_request=1',
+            '-dxdebug.client_host=127.0.0.1',
+            '-dxdebug.client_port=9003',
+        ),
+        '-dzend.assertions=1',
+        '-f', $utilPathname, '--',
         $argv
     ));
     fprintf(
         STDERR,
         "debug: xdebug cli session (server-name: '%s')\n",
-        $name
+        $serverName
     );
     passthru(
-        'XDEBUG_CONFIG="" PHP_IDE_CONFIG="serverName=' . $name . '-cli" ' .
+        'XDEBUG_CONFIG="" PHP_IDE_CONFIG="serverName=' . $serverName . '" ' .
         $cmd,
         $return_var
     );
